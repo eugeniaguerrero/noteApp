afa86a9ca8b8f7af5f7eeae0301dbe06a6df0886
Eugenia Guerrero <eugenia.guerrero@me.com>
2017-04-26 11:01:10 +0100

    updates the code to only show the first 20 characters of each note in the list

--- tests/note-list-view-test.js
+++ tests/note-list-view-test.js
@@ -4,4 +4,12 @@ function listViewTest() {
   var listView = new ListView(list);
   assert.isTrue(listView.htmlConverter() === '<ul><li>Euge</li></ul>')
 };
+function truncatedListViewTest(){
+  var list = new List();
+  list.addNote("The three stood listening to a fresh access; Of wind that caught against the house a moment");
+  var listView = new ListView(list);
+  assert.isTrue(listView.htmlConverter() === '<ul><li>The three stood list</li></ul>')
+  };
+
 listViewTest();
+truncatedListViewTest();
